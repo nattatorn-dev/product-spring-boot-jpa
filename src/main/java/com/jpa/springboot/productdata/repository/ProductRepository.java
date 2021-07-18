e7692fb5e28136a994e00d534aa63fd2c64994d1
Nattatorn Yucharoen <nattatorn.yuch@Nattatorns-MacBook-Pro.local>
2021-07-18 21:29:29 +0700

    Example Repository finder

--- src/main/java/com/jpa/springboot/productdata/repository/ProductRepository.java
+++ src/main/java/com/jpa/springboot/productdata/repository/ProductRepository.java
@@ -1,8 +1,17 @@
 package com.jpa.springboot.productdata.repository;
 
+import java.util.List;
+
 import com.jpa.springboot.productdata.entity.Product;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
+  List<Product> findByName(String name);
+  List<Product> findByNameAndDesc(String name, String desc);
+  List<Product> findByPriceGreaterThan(Double price);
+  List<Product> findByDescContains(String desc);
+  List<Product> findByPriceBetween(Double priceFrom, Double priceTo);
+  List<Product> findByDescLike(String desc);
+  List<Product> findByIdIn(List<Integer> ids);
 }
--- src/test/java/com/jpa/springboot/productdata/ProductdataApplicationTests.java
+++ src/test/java/com/jpa/springboot/productdata/ProductdataApplicationTests.java
@@ -1,8 +1,10 @@
 package com.jpa.springboot.productdata;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Arrays;
+import java.util.List;
 
 import com.jpa.springboot.productdata.entity.Product;
 import com.jpa.springboot.productdata.repository.ProductRepository;
@@ -17,9 +19,9 @@ public class ProductdataApplicationTests {
   @Autowired
   ProductRepository repository;
 
-	@Test
-	public void contextLoads() {
-	}
+  @Test
+  public void contextLoads() {
+  }
 
   @Test
   public void testCreate() {
@@ -44,5 +46,48 @@ public class ProductdataApplicationTests {
     Product product = repository.findById(1).get();
     product.setPrice(1200d);
     repository.save(product);
+  }
+
+  @Test
+  public void testDelete() {
+    if (repository.existsById(1)) {
+      repository.deleteById(1);
+    }
+  }
+
+  @Test
+  public void testCound() {
+    repository.count();
+  }
+
+  @Test
+  public void testFindByPriceGreaterThan() {
+    List<Product> products = repository.findByPriceGreaterThan(500d);
+    products.forEach(p -> System.out.println(p.getPrice()));
+  }
+
+  @Test
+  public void testFindByDescContains() {
+    List<Product> products = repository.findByDescContains("Iphone");
+    products.forEach(p -> System.out.println(p.getName()));
+  }
+
+  @Test
+  public void testFindByPriceBetween() {
+    List<Product> products = repository.findByPriceBetween(900d, 50000d);
+    products.forEach(p -> System.out.println(p.getName()));
+  }
+
+  @Test
+  public void testFindByDescLike() {
+    List<Product> products = repository.findByDescLike("%max%");
+    products.forEach(p -> System.out.println(p.getName()));
+  }
+
+
+  @Test
+  public void testFindByIdIn() {
+    List<Product> products = repository.findByIdIn(Arrays.asList(1 ,2));
+    products.forEach(p -> System.out.println(p.getName()));
   }
 }
